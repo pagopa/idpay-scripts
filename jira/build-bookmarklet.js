12edc975d183855ec22a4f06378a2be5c6eecc68
EMORONGD6 <guido.moroni@emeal.nttdata.com>
2026-05-11 14:11:22 +0200

    feat: add scripts for jira

---
+++ jira/build-bookmarklet.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+/**
+ * Genera 2 bookmarklet da subtask.js:
+ *   1. subtask.bookmarklet.inline.txt  - script intero dentro il segnalibro
+ *   2. subtask.bookmarklet.loader.txt  - carica subtask.js da URL remoto
+ * USO:  node build-bookmarklet.js
+ */
+const fs = require('fs');
+const path = require('path');
+const SRC        = path.join(__dirname, 'view-subtask.js');
+const OUT_INLINE = path.join(__dirname, 'subtask.bookmarklet.inline.txt');
+const OUT_LOADER = path.join(__dirname, 'subtask.bookmarklet.loader.txt');
+// ⚠️ Cambia con l'URL raw pubblico del tuo subtask.js
+const REMOTE_URL = 'https://raw.githubusercontent.com/YOUR_ORG/YOUR_REPO/main/subtask.js';
+function minify(s) {
+  return s
+    .replace(/\/\*[\s\S]*?\*\//g, '')
+    .replace(/^\s*\/\/.*$/gm, '')
+    .replace(/[ \t]+/g, ' ')
+    .replace(/\n\s*\n/g, '\n')
+    .trim();
+}
+function toBookmarklet(code) {
+  const wrapped = "(function(){try{" + code +
+    "}catch(e){console.error('[Jira STE]',e);alert('[Jira STE] '+e.message);}})();void 0;";
+  return 'javascript:' + encodeURIComponent(wrapped);
+}
+const code = minify(fs.readFileSync(SRC, 'utf8'));
+fs.writeFileSync(OUT_INLINE, toBookmarklet(code) + '\n');
+console.log('OK ' + path.basename(OUT_INLINE));
+const loader =
+  "var s=document.createElement('script');" +
+  "s.src=" + JSON.stringify(REMOTE_URL) + "+'?t='+Date.now();" +
+  "s.onerror=function(){alert('[Jira STE] Impossibile caricare '+s.src);};" +
+  "document.head.appendChild(s);";
+fs.writeFileSync(OUT_LOADER, toBookmarklet(loader) + '\n');
+console.log('OK ' + path.basename(OUT_LOADER));
